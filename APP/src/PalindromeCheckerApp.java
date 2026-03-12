45572c00e14261350374181f186c1c17f25f0ad5
AviralAgarwal-001 <aviral13579@gmail.com>
2026-03-12 09:15:48 +0530

    Its Palindrome Checker App

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,38 +1,21 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.Stack;
 
-interface PalindromeStrategy {
-    boolean checkPalindrome(String input);
-}
-
-class StackStrategy implements PalindromeStrategy {
+class PalindromeChecker {
     public boolean checkPalindrome(String input) {
         Stack<Character> stack = new Stack<>();
+
         for (int i = 0; i < input.length(); i++) {
             stack.push(input.charAt(i));
         }
 
-        for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) != stack.pop()) {
-                return false;
-            }
-        }
-        return true;
-    }
-}
+        String reversed = "";
 
-class DequeStrategy implements PalindromeStrategy {
-    public boolean checkPalindrome(String input) {
-        Deque<Character> deque = new LinkedList<>();
-        for (int i = 0; i < input.length(); i++) {
-            deque.addLast(input.charAt(i));
+        while (!stack.isEmpty()) {
+            reversed += stack.pop();
         }
 
-        while (deque.size() > 1) {
-            if (deque.removeFirst() != deque.removeLast()) {
-                return false;
-            }
-        }
-        return true;
+        return input.equals(reversed);
     }
 }
 
@@ -42,18 +25,9 @@ public class PalindromeCheckerApp {
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
 
-        PalindromeStrategy strategy;
+        PalindromeChecker checker = new PalindromeChecker();
 
-        System.out.print("Choose Strategy (1-Stack, 2-Deque): ");
-        int choice = sc.nextInt();
-
-        if (choice == 1) {
-            strategy = new StackStrategy();
-        } else {
-            strategy = new DequeStrategy();
-        }
-
-        if (strategy.checkPalindrome(input)) {
+        if (checker.checkPalindrome(input)) {
             System.out.println("Palindrome");
         } else {
             System.out.println("Not a Palindrome");
